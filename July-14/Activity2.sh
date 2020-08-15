666e1e7a7e20d2bbef6da8bf5ee16337737480ca
Ray <32021785+fluentinstroll@users.noreply.github.com>
2020-08-15 10:53:04 -0400

    Add files via upload

---
+++ July-14/Activity2.sh
@@ -0,0 +1,16 @@
+add_a_user()
+{
+ USER=$1
+ PASSWORD=$2
+ shift; shift;
+ # Having shifted twice, the rest is now comments ...
+ COMMENTS=$@
+ echo "Adding user $USER ..."
+ echo useradd -c "$COMMENTS" $USER
+ echo passwd $USER $PASSWORD
+ echo "Added user $USER ($COMMENTS) with pass $PASSWORD"
+}
+
+#main script
+
+add_a_user ray rambo;
